e58f330d977d5336af974c39d9def117c0f1ecca
alessandronahon <alessandronahon@gmail.com>
2023-12-04 16:16:56 -0500

    tictactoe!

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,79 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the game', () => {
+  render(<App />)
+
+  const startHeader = screen.getByText(/Turn: o/i)
+  const board = screen.getByTestId("board")
+
+  expect(startHeader).toBeInTheDocument()
+  expect(board).toBeInTheDocument()
 });
+
+test('player O starts the game and makes the first move', () => {
+  render(<App />)
+  const firstSquare = screen.getByTestId(`row:0, col:0`);
+
+  fireEvent.click(firstSquare)
+
+  const squareContent = within(firstSquare)?.getByTestId('square')
+  const nextHeader = screen.getByText(/Turn: x/i)
+
+  expect(squareContent).toHaveTextContent("o")
+  expect(nextHeader).toBeInTheDocument()
+
+})
+
+test('player X makes the second move after player O', () => {
+  render(<App />)
+  const firstSquare = screen.getByTestId(`row:0, col:0`);
+  const secondSquare = screen.getByTestId(`row:0, col:1`);
+
+  fireEvent.click(firstSquare)
+  fireEvent.click(secondSquare)
+
+  const squareContent = within(secondSquare)?.getByTestId('square')
+  const nextHeader = screen.getByText(/Turn: o/i)
+
+  expect(squareContent).toHaveTextContent("x")
+  expect(nextHeader).toBeInTheDocument()
+
+})
+
+test('game is won by player O diagonally', () => {
+  render(<App />)
+
+  const firstSquare = screen.getByTestId(`row:0, col:0`);
+  const secondSquare = screen.getByTestId(`row:0, col:1`);
+  const thirdSquare = screen.getByTestId(`row:1, col:1`);
+  const fourthSquare = screen.getByTestId(`row:0, col:2`);
+  const fifthSquare = screen.getByTestId(`row:2, col:2`);
+
+  fireEvent.click(firstSquare)
+  fireEvent.click(secondSquare)
+  fireEvent.click(thirdSquare)
+  fireEvent.click(fourthSquare)
+  fireEvent.click(fifthSquare)
+
+  const winnerText = screen.getByText(/o is the winner!/i)
+  expect(winnerText).toBeInTheDocument()
+})
+
+test('player x clicks on already selected square and nothing happens', () => {
+  render(<App />)
+
+  const firstSquare = screen.getByTestId(`row:0, col:0`);
+
+  fireEvent.click(firstSquare)
+
+  const nextHeader = screen.getByText(/Turn: x/i)
+
+  expect(nextHeader).toBeInTheDocument()
+
+  fireEvent.click(firstSquare)
+
+  expect(nextHeader).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
+type Board = string[][]
+
+type Shape = 'x' | 'o'
+
+type Player = {
+  shape: Shape
+}
+
+const defaultBoard: Board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+]
+
+const player1: Player = {
+  shape: 'o'
+}
+
+const player2: Player = {
+  shape: 'x'
+}
+
+const players = [player1, player2]
+
 function App() {
+  const [board, setBoard] = useState<Board>(defaultBoard)
+  const [player, setPlayer] = useState<Player>(player1)
+  const winner = useRef<string>('')
+
+  function selectSquare(row: number, col: number): void {
+    const updatedBoard: Board = board.map((r, i) => r.map((shape, j) => {
+
+      if (row === i && col === j && shape === '') {
+        if (player.shape === player1.shape) {
+          setPlayer(player2)
+        } else {
+          setPlayer(player1)
+        }
+        return player.shape
+      }
+      return shape
+    }))
+
+    if (winner.current === '') {
+      setBoard(updatedBoard)
+    }
+  }
+
+  function arrCol(arr: Board | { [x: string]: any; }[], n: number) {
+    return arr.map((x: { [x: string]: any; }) => x[n])
+  }
+
+  function tictactoe(arr: string[], p: Player): boolean {
+    return arr.every(c => c === p.shape)
+  }
+
+  function diagRow(b: Board): string[] {
+    return b.map((_, i) => b[i][i])
+  }
+
+  function reverseDiagRow(board: Board) {
+    return diagRow(board.map((row) => row.reverse()))
+  }
+
+  function findWinner() {
+    board.forEach((row, i) => {
+      const col = arrCol(board, i)
+      const diag = [reverseDiagRow(board), reverseDiagRow(board)]
+
+      players.forEach(p => {
+        if (tictactoe(row, p) || tictactoe(col, p) || tictactoe(diag[0], p) || tictactoe(diag[1], p)) {
+          winner.current = `${p.shape} is the winner!`
+        }
+      })
+    })
+  }
+
+  findWinner()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className='text-9xl mb-10'>{winner.current === '' ? `Turn: ${player.shape}` : winner.current}</h1>
+      <div data-testid='board' className='w-3/6 h-2/3 grid grid-rows-3 grid-cols-3 col-span-3 gap-3'>
+        {
+          board.map((row, i) => row.map((col, j) => (
+            <div data-testid={`row:${i}, col:${j}`} key={`row:${i}, col:${j}`} className='flex cursor-pointer items-center justify-center h-40 w-40 border-4' onClick={() => selectSquare(i, j)}>
+              <h2 data-testid='square' className='text-center text-8xl'>{col}</h2>
+            </div>
+          )))
+        }
+      </div>
+
     </div>
   );
 }
